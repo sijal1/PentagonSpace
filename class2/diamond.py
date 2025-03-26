c179393152f8c2201835039d100c3693db14d69a
sijal1 <muhammadsijal2003@gmail.com>
2025-03-26 14:56:42 +0530

    Daily update

--- class2/diamond.py
+++ class2/diamond.py
@@ -1,21 +1,17 @@
-n=3
+n = 3
 
-for i in range(1,n+1,1):
-
-    for j in range(1,n-i+1+1,1):
-        print(" ",end="")   
-
-    for j in range(1,2*i-1+1,1):
-        print( "*",end="")   
+# Upper 
+for i in range(1, n + 1):
+    for j in range(1, n - i + 1 + 1):
+        print(" ", end="")   
+    for j in range(1, 2 * i - 1 + 1):
+        print("*", end="")   
     print("")     
-n=2
-for i in range(1,n+1,1):
 
-    for j in range(1,n-i+1,1):
-        print(" ",end="")      
-
-    for j in range(1,n-i+1+1,1):
-        print("*",end="")     
-
-    print("")   
-
+# Lower
+for i in range(n - 1, 0, -1):
+    for j in range(1, n - i + 1 + 1):
+        print(" ", end="")   
+    for j in range(1, 2 * i - 1 + 1):
+        print("*", end="")   
+    print("")  
