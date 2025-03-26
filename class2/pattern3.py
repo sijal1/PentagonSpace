0f6b07306212a2e8acaafbbcd379bf32cd078b97
sijal1 <muhammadsijal2003@gmail.com>
2025-03-26 23:58:09 +0530

    Daily update

--- class2/pattern3.py
+++ class2/pattern3.py
@@ -28,9 +28,12 @@ for i in range(1,5+1 ,1):
     print("")       
 
 for i in range(1,3+1,1):
-    for j in range(1,n-i+1,1):
+    for j in range(1,n-i-1+1,1):
         print(" ",end="")
     for j in range(1,i+1+1,1):
         print("*",end="")
-        
+    for j in range(1,2+1,1):    
+        print("O",end="")      
+    for j in range(1,i+1+1,1):
+        print("*",end="")    
     print("")      
