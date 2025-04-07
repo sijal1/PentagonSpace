082d8a15dc48be3c011ad0632191d535ff7e5405
sijal1 <muhammadsijal2003@gmail.com>
2025-04-07 21:21:17 +0530

    Daily update

--- class2/gfg1.py
+++ class2/gfg1.py
@@ -12,16 +12,3 @@ for i in range(1,4+1,1):
         print(a,end="")  
         a+=1  
     print("")  
-n=3    
-for i in range(1,3+1,1):
-    for j in range(1,3-i,1):
-        print(" ",end="")
-    a=i
-    for j in range(1,i+1,1):
-        print(a,end="")    
-        a-=1
-    a=2    
-    for j in range(1,i-1+1,1):
-        print(a,end="")  
-        a+=1  
-    print("")          
