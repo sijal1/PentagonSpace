d1a7a24f81c09484374c86068a0f3a66c18b697c
sijal1 <muhammadsijal2003@gmail.com>
2025-04-11 15:02:03 +0530

    class1

---
+++ class1/slicing1.py
@@ -0,0 +1,20 @@
+str="lenovoLAPTOP"
+print(str)
+print(len(str))     #12
+print(str[2:6])     #novo
+print(str[-2:6])    #no o/p
+print(str[2:8:-1])  #no o/p
+print(str[7:2])     #no o/p
+print(str[7:2:-2])  #Aoo
+print(str[-10:-7])  #nov
+print(str[ : : ])   #lenovoLAPTOP
+print(str[2:10:2])  #nvLP
+print(str[-2:-7])   #no o/p
+print(str[ : : -1]) #reverse
+print(str[4:-7])    #v
+print(str[3:10:4])  #oA
+
+
+
+
+# print("hello")
---
+++ class1/stringmath.py
@@ -0,0 +1,21 @@
+""" addition """
+
+str1="adam"
+str2="siri"
+str3=str1+str2
+print(str3)
+
+""" subtraction """
+# str4=str1-str2  #error
+#print(str4)
+
+""" multiplication """
+#str5=str1*str2   #error
+#print(str5)
+str6=str1*2
+print(str6)
+
+""" division """
+#str7=str1/str2   #error
+#print(str7)      #error
+
