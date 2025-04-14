62a207cd37839e03767b357375c6adc7e7ee777b
sijal1 <muhammadsijal2003@gmail.com>
2025-04-14 16:44:10 +0530

    Daily update

---
+++ class2/list4.py
@@ -0,0 +1,10 @@
+#sum of all elem in list
+n=int(input("enter n : "))
+a=[]
+print("enter the numbers : ")
+a=[int(input()) for i in range(n)]                                                             
+print("")
+sum=0
+for i in a :
+    sum=sum+i
+print(sum)
